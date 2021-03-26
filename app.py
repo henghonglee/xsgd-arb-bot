93e473c006ad2948138d398cacbfb1eab2142503
henghonglee <henghong.lee@gmail.com>
2021-03-26 19:20:17 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import subprocess
+import schedule
+import time
+
+def job():
+    subprocess.run(["scrapy", "runspider", "crawler/spiders/xsgd_price.py"])
+
+# schedule.every(10).seconds.do(job)
+schedule.every(1).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every(5).to(10).minutes.do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ crawler/spiders/xsgd_price.py
@@ -0,0 +1,41 @@
+import scrapy
+import pdb
+import json
+import telegram
+import os.path
+
+class XsgdPriceSpider(scrapy.Spider):
+    name = 'xsgd_price'
+    allowed_domains = ['https://zilstream.com/tokens/xsgd']
+    start_urls = ['https://zilstream.com/tokens/xsgd/']
+    
+
+    def parse(self, response):
+        response_body = response.body.decode("utf-8")
+        start = "{\"props\""
+        start_idx = response_body.rfind(start)
+        end = "}</script>"
+        end_idx = response_body.rfind(end) + 1
+        json_response = json.loads(response_body[start_idx:end_idx])
+        rate_usd = json_response['props']['pageProps']['token']['rate_usd']
+
+        file = open('xsgd_price.txt', 'a')
+        file.write("{:.15f}\n".format(rate_usd))
+        file.close()
+
+        file = open('xsgd_price.txt', 'r')
+        lines = file.readlines()
+        file.close()
+
+        for line in lines:
+            v = float(line.strip('\n'))
+            d = abs(rate_usd - v)
+            if d > 0.01:
+                percentage = (rate_usd - v)/rate_usd * 100
+                bot = telegram.Bot(token='1774766230:AAFJ8r2cf5P6gidpRgcH8-UGQPYoHrZ0b-8')
+                bot.send_message(687804705, "XSGD price alert({:.3}%): {:.4f} -> {:.4f}".format(percentage, v, rate_usd))
+                print("removing file")
+                os.remove('xsgd_price.txt')
+                break
+        
+        
