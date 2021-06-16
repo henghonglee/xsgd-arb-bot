15b9f5106f964391732a14e020f469be94f648ac
henghonglee <henghong.lee@gmail.com>
2021-06-16 15:14:48 +0800

    Add circ supply titan

--- crawler/spiders/titan_token_report_supply.py
+++ crawler/spiders/titan_token_report_supply.py
@@ -1,9 +1,10 @@
 import scrapy
+from scrapy.selector import Selector
 import telegram
 
 class TitanTokenSupplySpider(scrapy.Spider):
     name = 'titan_token_report_supply'
-    allowed_domains = ['https://polygonscan.com/']
+    allowed_domains = ['polygonscan.com/']
     start_urls = ['https://polygonscan.com/token/0xaaa5b9e6c589642f98a1cda99b9d024b8407285a']
     max_retries = 2
     
@@ -14,7 +15,21 @@ class TitanTokenSupplySpider(scrapy.Spider):
         end = "\" /></div>"
         end_idx = response_body.rfind(end)
         supply_string = response_body[start_idx:end_idx].replace(',','')
-        supply_val = round(float(supply_string), 2)
+        supply_val = float(supply_string)
+        print(supply_val)
+
+        request = scrapy.Request('https://polygonscan.com/token/generic-tokenholders2?m=normal&a=0xaaa5b9e6c589642f98a1cda99b9d024b8407285a&s=104658925627917937125433346&p=1',
+                             callback=self.parse_page2,
+                             cb_kwargs=dict(main_url=response.url),
+                             dont_filter=True)
+        request.cb_kwargs['supply_val'] = supply_val  # add more arguments for the callback
+        yield request
+        
+    def parse_page2(self, response, main_url, supply_val):
+        bank_supply_string = response.xpath("//*[@id=\"maintable\"]/div[3]/table/tbody/tr[1]/td[3]/text()").getall()[0].replace(',','')
+        bank_supply_val = float(bank_supply_string)
+        circ_supply_val = supply_val - bank_supply_val
+        percentage = (circ_supply_val / supply_val) * 100
         bot = telegram.Bot(token='1857941381:AAHPfQwENHSdjxmSyEJt0wutj8PDLZwf81Y')
-        bot.send_message(-503777814, "TITAN Supply {:,}".format(supply_val))
+        bot.send_message(-503777814, "TITAN Circulating Supply {:,} ({:.3f}%)".format(circ_supply_val, percentage))
         
--- crawler/spiders/titan_token_supply.py
+++ crawler/spiders/titan_token_supply.py
@@ -3,11 +3,11 @@ import telegram
 import os.path
 
 class TitanTokenSupplySpider(scrapy.Spider):
-    name = 'titan_token_supply'
-    allowed_domains = ['https://polygonscan.com/']
+    name = 'titan_token_report_supply'
+    allowed_domains = ['polygonscan.com/']
     start_urls = ['https://polygonscan.com/token/0xaaa5b9e6c589642f98a1cda99b9d024b8407285a']
     max_retries = 2
-
+    
     def parse(self, response):
         response_body = response.body.decode("utf-8")
         start = "id=\"ContentPlaceHolder1_hdnTotalSupply\" value=\""
@@ -15,23 +15,36 @@ class TitanTokenSupplySpider(scrapy.Spider):
         end = "\" /></div>"
         end_idx = response_body.rfind(end)
         supply_string = response_body[start_idx:end_idx].replace(',','')
-        supply_float = round(float(supply_string), 2)
+        supply_val = float(supply_string)
+
+        request = scrapy.Request('https://polygonscan.com/token/generic-tokenholders2?m=normal&a=0xaaa5b9e6c589642f98a1cda99b9d024b8407285a&s=104658925627917937125433346&p=1',
+                             callback=self.parse_page2,
+                             cb_kwargs=dict(main_url=response.url),
+                             dont_filter=True)
+        request.cb_kwargs['supply_val'] = supply_val  # add more arguments for the callback
+        yield request
+        
+    def parse_page2(self, response, main_url, supply_val):
+        bank_supply_string = response.xpath("//*[@id=\"maintable\"]/div[3]/table/tbody/tr[1]/td[3]/text()").getall()[0].replace(',','')
+        bank_supply_val = float(bank_supply_string)
+        circ_supply_val = supply_val - bank_supply_val
+        percentage = (circ_supply_val / supply_val) * 100
+        
 
         file = open('titan_token.txt', 'a')
-        file.write("{:.15f}\n".format(supply_float))
+        file.write("{:.15f}\n".format(circ_supply_val))
         file.close()
 
         file = open('titan_token.txt', 'r')
         lines = file.readlines()
         file.close()
-
+        
         for line in lines:
             v = float(line.strip('\n'))
-            percentage = (supply_float - v)/supply_float * 100
-            if percentage > 3:
+            percentage = (circ_supply_val - v)/circ_supply_val * 100
+            if percentage > 0.001:
                 bot = telegram.Bot(token='1857941381:AAHPfQwENHSdjxmSyEJt0wutj8PDLZwf81Y')
-                bot.send_message(-503777814, "TITAN token change alert({:.3}%): {:.4f} -> {:.4f}".format(percentage, v, supply_float))
+                bot.send_message(-503777814, "TITAN circ supply change alert({:.3}%): {:.4f} -> {:.4f}".format(percentage, v, circ_supply_val))
                 os.remove('titan_token.txt')
                 break
-        
         
